c9aa94262757aa4f4500d248c0f7c9e9c3bad356
Matt <matthew.vanegas@columbiaspectator.com>
2019-08-21 11:03:40 -0400

    Creation of Container to demonstrate functionality of ToC

--- src/containers/Orientation.jsx
+++ src/containers/Orientation.jsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import React, { Component } from 'react';
-import TableOfContents from '../components/TableOfContents';
 
 const Container = styled.div``;
 
@@ -23,117 +22,17 @@ export default class Orientation extends Component {
     this.state = {};
   }
 
-  generateEntries = () => {
-    const entries = [];
-    for (let i = 0; i < 12; i += 1) {
-      entries.push(`yeet${i}`);
-    }
-    return entries;
-  };
-
   render() {
     return (
       <Container>
         <Row>
           <WelcomeColumn>
             <WelcomeContainer>
-              <div style={{ height: '100vh', width: '100vw' }}>
-                {' '}
-                <TableOfContents entries={this.generateEntries()} />
-                {' '}
+              <div>
+                <h1>Welcome</h1>
+                <h2>Class of 2023</h2>
+                <p>Select an option below to explore your orientation guide</p>
               </div>
-              <div
-                id="yeet0"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'green',
-                }}
-              >
-                Landing
-                {' '}
-              </div>
-              <div
-                id="yeet1"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'red',
-                }}
-              >
-                {' '}
-                Section 1
-                {' '}
-              </div>
-              <div
-                id="yeet2"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'blue',
-                }}
-              >
-                {' '}
-                Section 2
-              </div>
-              <div
-                id="yeet3"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'yellow',
-                }}
-              >
-                {' '}
-                Section 3
-              </div>
-              <div
-                id="yeet4"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'magenta',
-                }}
-              >
-                {' '}
-                Section 4
-              </div>
-              <div
-                id="yeet5"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'cyan',
-                }}
-              >
-                {' '}
-                Section 5
-              </div>
-              <div
-                id="yeet6"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'purple',
-                }}
-              >
-                {' '}
-                Section 6
-              </div>
-              <div
-                id="yeet7"
-                style={{
-                  height: '100vh',
-                  width: '100vw',
-                  backgroundColor: 'brown',
-                }}
-              >
-                {' '}
-                Section 7
-              </div>
-              <h1>Welcome</h1>
-              <h2>Class of 2023</h2>
-              <p>Select an option below to explore your orientation guide</p>
             </WelcomeContainer>
           </WelcomeColumn>
           <WelcomeColumn />
