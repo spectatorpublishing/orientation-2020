2e860276f1bd32d66d41b5ae0966416c428f1f6d
Arsalaan <aaa2325@columbia.edu>
2019-08-24 04:33:54 -0400

    Production build

---
+++ build/precache-manifest.8f85f49fb548227953e386d85ee3d9af.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85c430eac904f6f0f4d001a1da0eb7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "152d8189a46c07baaf6a",
+    "url": "/static/css/main.00976162.css"
+  },
+  {
+    "revision": "152d8189a46c07baaf6a",
+    "url": "/static/js/main.0ddea1f1.js"
+  }
+]);
