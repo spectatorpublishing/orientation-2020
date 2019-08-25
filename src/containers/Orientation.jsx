77db24af74a1d20198d448364f234b620ea0e16a
Arsalaan <aaa2325@columbia.edu>
2019-08-25 16:47:12 -0400

    editor in chief

--- src/containers/Orientation.jsx
+++ src/containers/Orientation.jsx
@@ -213,7 +213,7 @@ const StaffInfoMobile = [
   {
     title: 'Corporate Board',
     members: [
-      'Katherine Gerberich, Editor And Chief',
+      'Katherine Gerberich, Editor in Chief',
       'Rahil Kamath, Managing Editor',
       'Nima Mozhgani, Publisher',
     ],
