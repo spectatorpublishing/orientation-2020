634f117bcddf98152491eae70e4f628a6b2260be
McKenna Gillard <mckennagrace121@gmail.com>
2020-08-30 16:51:20 -0400

    updated discourse and package json

--- src/data/JoinTheDiscourseData.js
+++ src/data/JoinTheDiscourseData.js
@@ -1,24 +1,24 @@
 const JoinTheDiscourseData = [
   {
     link:
-      'https://www.columbiaspectator.com/opinion/2019/08/22/letter-from-the-editors-learning-from-your-peers/',
+      'https://www.columbiaspectator.com/opinion/2020/04/21/discourse-and-debate-are-we-bad-new-yorkers/',
     photoUrl:
-      'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/CBZF6WPVQZE7VP4YWP3A2BQHJI.gif',
-    headline: 'Letter from the Editors: Learning from your peers',
+      'https://cloudfront-us-east-1.images.arcpublishing.com/spectator/TFDXCK62ENG65LVAS46KI6F35Y.png',
+    headline: 'Discourse and Debate: Are we “bad” New Yorkers?',
   },
   {
     link:
-      'https://www.columbiaspectator.com//opinion/2019/08/22/a-case-against-dry-shampoo-and-social-comparison/',
+      'https://www.columbiaspectator.com/opinion/2020/07/28/our-education-is-founded-on-white-supremacy-a-conscious-re-education-is-necessary/',
     photoUrl:
-      'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/V7YXVXHWZ5HXTBCI2V5BXUMSAA.jpg',
-    headline: 'A case against dry shampoo and social comparison',
+      'https://cloudfront-us-east-1.images.arcpublishing.com/spectator/7LQMJ2YLQ5GDDMOWFAQWBEL3FI.JPG',
+    headline: 'Our education was founded on white supremacy. A conscious re-education is necessary.',
   },
   {
     link:
-      'https://www.columbiaspectator.com/opinion/2019/05/03/to-the-class-of-2023/',
+      'https://www.columbiaspectator.com/opinion/2020/03/29/100-words-we-asked-how-covid-19-has-impacted-students-lives-here-are-their-responses/',
     photoUrl:
-      'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KDHYR64SWNAK5AYLKWEPFCJEPA.png',
-    headline: 'To the class of 2023',
+      'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/6MTSNPXPPJEQRPTZFYEDZHPKOE.png',
+    headline: '[100 Words]: We asked how COVID-19 has impacted students\' lives. Here are their responses.',
   },
 ];
 
