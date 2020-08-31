1f3eb52f1c14a9da49e8fc2182223feb4bbeea8f
McKenna Gillard <mckennagrace121@gmail.com>
2020-08-31 09:16:26 -0400

    updated discourse

--- src/data/JoinTheDiscourseData.js
+++ src/data/JoinTheDiscourseData.js
@@ -11,14 +11,16 @@ const JoinTheDiscourseData = [
       'https://www.columbiaspectator.com/opinion/2020/07/28/our-education-is-founded-on-white-supremacy-a-conscious-re-education-is-necessary/',
     photoUrl:
       'https://cloudfront-us-east-1.images.arcpublishing.com/spectator/7LQMJ2YLQ5GDDMOWFAQWBEL3FI.JPG',
-    headline: 'Our education was founded on white supremacy. A conscious re-education is necessary.',
+    headline:
+      'Our education was founded on white supremacy. A conscious re-education is necessary.',
   },
   {
     link:
       'https://www.columbiaspectator.com/opinion/2020/03/29/100-words-we-asked-how-covid-19-has-impacted-students-lives-here-are-their-responses/',
     photoUrl:
       'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/6MTSNPXPPJEQRPTZFYEDZHPKOE.png',
-    headline: '[100 Words]: We asked how COVID-19 has impacted students\' lives. Here are their responses.',
+    headline:
+      "[100 Words]: We asked how COVID-19 has impacted students' lives. Here are their responses.",
   },
 ];
 
