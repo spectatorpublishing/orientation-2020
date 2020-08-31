d6ed742db3ff0f83e84cf61a7e123d79d5810a70
McKenna Gillard <mckennagrace121@gmail.com>
2020-08-31 09:29:48 -0400

    Updates

---
+++ precache-manifest.2337c19f599f5e02b7a5283c0586215f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba0c8d88a8c7dc15d83197dd1f4f42a",
+    "url": "/orientation-2020/index.html"
+  },
+  {
+    "revision": "7d0355de2ba78d5a6b02",
+    "url": "/orientation-2020/static/css/main.ec34ea6c.css"
+  },
+  {
+    "revision": "7d0355de2ba78d5a6b02",
+    "url": "/orientation-2020/static/js/main.a28feb57.js"
+  }
+]);
